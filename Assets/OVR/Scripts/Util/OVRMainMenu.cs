48f665fa6ff2a12d3d5d692e7359c3c304479d7d
weiping <workaholic.angel.86@gmail.com>
2015-04-22 10:37:59 +0800

    Unity 5.0.1 still does not have the beta changes.

--- Assets/OVR/Scripts/Util/OVRMainMenu.cs
+++ Assets/OVR/Scripts/Util/OVRMainMenu.cs
@@ -244,7 +244,7 @@ public class OVRMainMenu : MonoBehaviour
 	        r.localEulerAngles = Vector3.zero;
 
 			Canvas c = NewGUIObject.AddComponent<Canvas>();
-#if (UNITY_5_0_1)
+#if (UNITY_5_0_2)
 			// TODO: Unity 5.0b11 has an older version of the new GUI being developed in Unity 4.6.
 			// Remove this once Unity 5 has a more recent merge of Unity 4.6.
 	        c.renderMode = RenderMode.World;
@@ -963,7 +963,7 @@ public class OVRMainMenu : MonoBehaviour
         r.localEulerAngles = Vector3.zero;
         r.localScale = new Vector3(0.001f, 0.001f, 0.001f);
         Canvas c = RiftPresentGUIObject.AddComponent<Canvas>();
-#if UNITY_5_0_1
+#if UNITY_5_0_2
 		// TODO: Unity 5.0b11 has an older version of the new GUI being developed in Unity 4.6.
 	   	// Remove this once Unity 5 has a more recent merge of Unity 4.6.
 		c.renderMode = RenderMode.World;
